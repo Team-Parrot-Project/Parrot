b88b5a870f0d5a479f9384e0e82f39975d431f4a
bird <mbird3@gmail.com>
2023-05-08 09:34:07 -0700

    add csrf route

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
+
+const csurf = require('csurf');
+
 // var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/api/users');
+const csrfRouter = require('./routes/api/csrf');
 
 var app = express();
 
@@ -14,7 +20,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
 // app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
