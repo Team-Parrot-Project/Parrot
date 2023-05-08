3605e067305fce0bb2e723390794a1f87c89f5ac
bird <mbird3@gmail.com>
2023-05-08 16:39:27 -0700

    initial seed file working

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -1,57 +1,116 @@
 const mongoose = require("mongoose");
 const { mongoURI: db } = require('../config/keys.js');
 const User = require('../models/User');
+// const Task = require('../models/Task');
+const {Project, Task} = require('../models/Project.js');
+// const Project = require('../models/Project');
 const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker');
 
-const NUM_SEED_USERS = 10;
 
-// Create users
-const users = [];
+  // sdkfjas
 
-users.push(
-  new User ({
-    username: 'demo-user',
-    email: 'demo-user@appacademy.io',
-    hashedPassword: bcrypt.hashSync('starwars', 10)
-  })
-)
-
-for (let i = 1; i < NUM_SEED_USERS; i++) {
-  const firstName = faker.name.firstName();
-  const lastName = faker.name.lastName();
-  users.push(
-    new User ({
-      username: faker.internet.userName(firstName, lastName),
-      email: faker.internet.email(firstName, lastName),
-      hashedPassword: bcrypt.hashSync(faker.internet.password(), 10)
-    })
-  )
-}
-
-const insertSeeds = () => {
-    console.log("Resetting db and seeding users and tweets...");
-
-    User.collection.drop()
-        .then(() => User.insertMany(users))
-        .then(() => {
-            console.log("Done!");
-            mongoose.disconnect();
-        })
-        .catch(err => {
-            console.error(err.stack);
-            process.exit(1);
-        });
-}
-
-// Connect to database
-mongoose
+  mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to MongoDB successfully');
-    insertSeeds();
-  })
-  .catch(err => {
-    console.error(err.stack);
-    process.exit(1);
-  });
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+const seedDB = async () => {
+  try {
+    // Create users
+    const users = [];
+
+    const hashedPassword = await bcrypt.hash('password', 10);
+
+    users.push(
+      new User({
+        email: 'admin@example.com',
+        username: 'admin',
+        hashedPassword
+      })
+    );
+
+    for (let i = 1; i <= 10; i++) {
+      const email = faker.internet.email();
+      const username = faker.internet.userName();
+      const hashedPassword = await bcrypt.hash('password', 10);
+
+      users.push(
+        new User({
+          email,
+          username,
+          hashedPassword
+        })
+      );
+    }
+
+    // Save users to database
+    const savedUsers = await User.insertMany(users);
+
+    // Create tasks
+    const tasks = [];
+
+    for (let i = 1; i <= 50; i++) {
+      const title = faker.lorem.words();
+      const description = faker.lorem.paragraph();
+      const priority = "medium";
+      const assignee = savedUsers[Math.floor(Math.random() * 10)]._id;
+      const status = "in progress";
+      const startDate = faker.date.past();
+      const endDate = faker.date.future();
+      const blockingTasks = [];
+
+      tasks.push(
+        new Task({
+          title,
+          description,
+          priority,
+          assignee,
+          status,
+          startDate,
+          endDate,
+          blockingTasks
+        })
+      );
+    }
+
+    // Save tasks to database
+    const savedTasks = await Task.insertMany(tasks);
+
+    // Create projects
+    const projects = [];
+
+    for (let i = 1; i <= 5; i++) {
+      const title = faker.company.catchPhrase();
+      const description = faker.lorem.sentences();
+      const adminId = savedUsers[0]._id;
+      const collaborator = savedUsers[Math.floor(Math.random() * 10)]._id;
+      const projectTasks = savedTasks.slice((i - 1) * 10, i * 10);
+      const startDate = faker.date.past();
+      const endDate = faker.date.future();
+
+      projects.push(
+        new Project({
+          title,
+          description,
+          adminId,
+          collaborators: [collaborator],
+          tasks: projectTasks,
+          startDate,
+          endDate
+        })
+      );
+    }
+
+    // Save projects to database
+    await Project.insertMany(projects);
+
+    console.log('Database seeded!');
+  } catch (err) {
+    console.log(err);
+  } finally {
+    mongoose.disconnect();
+  }
+};
+
+seedDB();
