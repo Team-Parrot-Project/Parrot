67a48c16dfaa758b67a849f1f57ebab870338b3a
chrisbanas <bananas595@gmail.com>
2023-05-09 09:12:36 -0700

    basic style for user home page started

--- frontend/src/components/UserHome/Dashboard/Dashboard.js
+++ frontend/src/components/UserHome/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CurrentDate from './Date/Date';
 import './Dashboard.css';
 
 export default function Dashboard() {
@@ -10,11 +11,11 @@ export default function Dashboard() {
     <>
 
       <div className="user-dashboard-wrapper">
-        <div className="main-content">
-          <header>
-            <div className="header-content">
-              <h2>Sunday, May 7</h2>
-              <h1>Good evening, Christopher</h1>
+        <div className="user-dashboard-main-content">
+          <header className="user-dashboard-header">
+            <div className="user-dashboard-header-content">
+              <h2 className="user-dashboard-date"><CurrentDate /></h2>
+              <h1 className="user-dashboard-greeting">Good evening, Christopher</h1>
             </div>
           </header>
           <div className="dropdown-container">
@@ -23,18 +24,18 @@ export default function Dashboard() {
               <option value="week">Week</option>
               <option value="month">Month</option>
             </select>
-            <div className="stats">
-              <p>Tasks completed: <span>5</span></p>
-              <p>Collaborators: <span>3</span></p>
+            <div className="user-dashboard-project-quick-stats">
+              <p className="user-dashboard-project-quick-stats-title">Tasks completed: <span>5</span></p>
+              <p className="user-dashboard-project-quick-stats-title">Collaborators: <span>3</span></p>
             </div>
           </div>
-          <div className="content">
-            <div className="tasks">
-              <h2>Tasks</h2>
+          <div className="user-dashboard-project-task-container">
+            <div className="user-dashboard-tasks-container">
+              <h2 className="user-dashboard-project-task-container-title">Tasks</h2>
 
             </div>
-            <div className="projects">
-              <h2>Projects</h2>
+            <div className="user-dashboard-projects-container">
+              <h2 className="user-dashboard-project-task-container-title">Projects</h2>
 
             </div>
           </div>
