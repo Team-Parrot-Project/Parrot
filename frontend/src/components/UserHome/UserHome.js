d172faf7fde827668e0d8ce467c7ceb87400992e
Roderick Mendoza <rodmendoza07@gmail.com>
2023-05-09 10:59:00 -0700

    update UserHome

--- frontend/src/components/UserHome/UserHome.js
+++ frontend/src/components/UserHome/UserHome.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import Dashboard from './Dashboard/Dashboard';
-import ProjectCreateForm from "./ProjectCreateForm/ProjectCreateForm";
-import TaskCreateForm from "./TaskCreateForm/TaskCreateForm";
 import './UserHome.css';
 
 export default function UserHome () {
@@ -14,8 +12,6 @@ export default function UserHome () {
       <div className="user-home-wrapper">
         <NavBar />
         <Dashboard/>
-        {/* <ProjectCreateForm />
-        <TaskCreateForm /> */}
       </div>
       </>
     );
