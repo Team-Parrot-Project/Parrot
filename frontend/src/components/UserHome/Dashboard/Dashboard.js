62f395e0974a0b633a2240e104af4fdd61695fad
chrisbanas <bananas595@gmail.com>
2023-05-09 17:50:32 -0700

    style updates to home

--- frontend/src/components/UserHome/Dashboard/Dashboard.js
+++ frontend/src/components/UserHome/Dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import './Dashboard.css';
 
 export default function Dashboard() {
 
+
+
   return (
 
 
@@ -25,15 +27,20 @@ export default function Dashboard() {
           <div className="dropdown-container">
 
             <div className="parent-user-dashboard-widget-select-date-container">
-              <div className="child-user-dashboard-widget-select-date-container" role="button" tabIndex="0">
-                <span className="user-dashboard-widget-select-date">My week
-
-                </span>
-                <svg className="user-dashboard-widget-select-date-icon" viewBox="0 0 24 24">
-                  <path
-                    d="M3.5,8.9c0-0.4,0.1-0.7,0.4-1C4.5,7.3,5.4,7.2,6,7.8l5.8,5.2l6.1-5.2C18.5,7.3,19.5,7.3,20,8c0.6,0.6,0.5,1.5-0.1,2.1 l-7.1,6.1c-0.6,0.5-1.4,0.5-2,0L4,10.1C3.6,9.8,3.5,9.4,3.5,8.9z">
-                  </path>
-                </svg>
+              <div className="child-user-dashboard-widget-select-date-container">
+                <div className="parent-user-dashboard-widget-select-date-container">
+                  <div className="child-user-dashboard-widget-select-date-container">
+                    <div className="dashboard-widget">
+                      <select
+                        className="user-dashboard-widget-select-date">
+                        <option value="my_week">My week</option>
+                        <option value="upcoming">Upcoming</option>
+                        <option value="overdue">Overdue</option>
+                        <option value="completed">Completed</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
 
