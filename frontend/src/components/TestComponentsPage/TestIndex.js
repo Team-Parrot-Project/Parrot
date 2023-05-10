64c7531f582290b887ee4906b19340a1e6024a36
Roderick Mendoza <rodmendoza07@gmail.com>
2023-05-10 12:17:39 -0700

    implemented modal for project create

--- frontend/src/components/TestComponentsPage/TestIndex.js
+++ frontend/src/components/TestComponentsPage/TestIndex.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProjectCreateForm from '../Project_CRUD/ProjectCreateForm/ProjectCreateForm';
+import ProjectCreateModal from '../Project_CRUD/ProjectCreateForm';
 import TaskCreateForm from '../Task_CRUD/TaskCreateForm/TaskCreateForm';
 import ProjectShow from '../Project_CRUD/ProjectShow/ProjectShow';
 import ProjectUpdateForm from '../Project_CRUD/ProjectUpdateForm/ProjectUpdateForm';
@@ -7,19 +7,20 @@ import TaskRecomendation from '../UserHome/TaskRecommendation/TaskRecommendation
 import ProjectDelete from '../Project_CRUD/ProjectDelete/ProjectDelete';
 import TaskShow from '../Task_CRUD/TaskShow/TaskShow';
 import TaskUpdateForm from '../Task_CRUD/TaskUpdateForm/TaskUpdateForm';
+
 import './TestIndex.css';
 
 export default function TestPage () {
   return (
       <div className="testingComponents">
-        <ProjectCreateForm />
-        <ProjectShow projectId="645b1d32fb7e41fd035603a9"/>
+        <ProjectCreateModal/>
+        {/* <ProjectShow projectId="645b1d32fb7e41fd035603a9"/>
         <ProjectUpdateForm projectId="645b1d32fb7e41fd035603a9"/>
         <ProjectDelete/>
         <TaskCreateForm />
         <TaskRecomendation title="Productivity App" description="The goal of this project is to develop a producitivty web app utilizing a MERN stack" startDate="2023-05-09T00:00:00.000Z" endDate="2023-05-10T00:00:00.000Z" />
         <TaskShow taskId="645bd51881ad6e27a7da1e9c"/>
-        <TaskUpdateForm taskId="645bd51881ad6e27a7da1e9c"/>
+        <TaskUpdateForm taskId="645bd51881ad6e27a7da1e9c"/> */}
       </div>
   );
 }
