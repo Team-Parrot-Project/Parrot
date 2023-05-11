202c14559d666e385a686470d480fc8a697ceadf
bird <mbird3@gmail.com>
2023-05-11 16:27:25 -0700

    implement tables and modal on user page

--- frontend/src/components/TableRow/TableRow.jsx
+++ frontend/src/components/TableRow/TableRow.jsx
@@ -1,7 +1,23 @@
 import './TableRow.css'
 
-export default function TableRow() {
+export default function TableRow({ cellClass, rowClass, row }) {
 
+    cellClass = cellClass || "defaulf-cell-class";
+    rowClass = rowClass || "default-row-class";
 
-    return <div className='bird-test'></div>
+    // return (<div className='bird-test'></div>)
+
+    return (
+        <>
+            <div className={rowClass}>
+                {row.map((cellContent, ix) => {
+                    return (
+                        <div key={ix} className={cellClass}>
+                            {cellContent}
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
 }
