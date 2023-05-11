abaf07e6a324c4a0e8e96080e66528ff11bffe80
chrisbanas <bananas595@gmail.com>
2023-05-11 14:31:11 -0700

    timeline complete

--- frontend/src/components/Timeline/FilterBar/FilterBar.js
+++ frontend/src/components/Timeline/FilterBar/FilterBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { setTimeframe } from '../../../store/timeframeActions';
+import { getProject } from '../../../store/project';
 import './FilterBar.css'
 
 export default function FilterBar() {
-
+  const { projectId } = useParams()
+  const project = useSelector(getProject(projectId));
   const dispatch = useDispatch();
 
   const handleTimeframeChange = (event) => {
@@ -12,10 +15,12 @@ export default function FilterBar() {
     dispatch(setTimeframe(selectedTimeframe));
   };
 
+  if (project) console.log(project.title)
 
   return (
     <>
       <div className="gantt-filter-bar">
+        <h1 className="timeline-project-title">{project?.title}</h1>
         <div className="gantt-filter-group">
           <label className="gantt-filter-group-title">Task:</label>
           <input type="text" id="gantt-task-filter" />
@@ -30,10 +35,10 @@ export default function FilterBar() {
         </div>
         <div className="gantt-filter-group">
           <label className="gantt-filter-group-title">Timeframe:</label>
-          <select id="gantt-timeframe-filter" onChange={handleTimeframeChange}>
+          <select defaultValue="month" id="gantt-timeframe-filter" onChange={handleTimeframeChange}>
             <option value="day">Day</option>
             <option value="week">Week</option>
-            <option value="month" selected>Month</option>
+            <option value="month">Month</option>
           </select>
         </div>
       </div>
