5d277865375bd5326953ec6d3869232927585df8
chrisbanas <bananas595@gmail.com>
2023-05-23 13:34:19 -0700

    cleanup code on project show

--- frontend/src/components/ProjectHome/ProjectHome.js
+++ frontend/src/components/ProjectHome/ProjectHome.js
@@ -16,40 +16,40 @@ import DeleteProjectModal from '../Project_CRUD/ProjectDelete/ProjectDeleteModal
 import { formatDate } from '../../store/util';
 import { fetchUsers } from '../../store/user';
 
-export default function ProjectHome () {
+export default function ProjectHome() {
   const dispatch = useDispatch();
-  const {projectId} = useParams();
+  const { projectId } = useParams();
   const [recommendedTasks, setRecommendedTasks] = useState([]);
   const project = useSelector((state) => state.projects[projectId]);
 
   useEffect(() => {
-      dispatch(taskActions.purgeTasks());
-      dispatch(projectActions.fetchProject(projectId));
-      dispatch(fetchUsers())
+    dispatch(taskActions.purgeTasks());
+    dispatch(projectActions.fetchProject(projectId));
+    dispatch(fetchUsers())
   }, [projectId, dispatch]);
 
-    return (
-      <>
+  return (
+    <>
       <div className="project-home-wrapper">
-        <NavBar/>
-      <div className="centered-container">
-        <h1 className="project-home-table-title">Project: {project?.title}</h1>
-        <h2 className="project-home-table-title">Description: {project?.description}</h2>
-        <h3 className="project-home-table-title">Start Date: {formatDate(project?.startDate)}</h3>
-        <h3 className="project-home-table-title">End Date: {formatDate(project?.endDate)}</h3>
+        <NavBar />
+        <div className="centered-container">
+          <h1 className="project-home-table-title">{project?.title}</h1>
+          <h2 className="project-home-table-title">{project?.description}</h2>
+          <h3 className="project-home-table-title">Start: {formatDate(project?.startDate)}</h3>
+          <h3 className="project-home-table-title">End: {formatDate(project?.endDate)}</h3>
 
-        <ProjectUpdateModal />
-        <TaskCreateModal />
-        <ProjectTaskIndex/>
-        <DeleteProjectModal />
-        <TaskRecommendation project={project} recommendedTasks={recommendedTasks} setRecommendedTasks={setRecommendedTasks}/>
-        <div className="task-create-forms">
-          {recommendedTasks.length > 0 && recommendedTasks.map((taskTitle) => (
-            <TaskCreateForm key={taskTitle} taskTitle={taskTitle} />
-          ))}
+          <ProjectUpdateModal />
+          <TaskCreateModal />
+          <DeleteProjectModal />
+          <ProjectTaskIndex />
+          <TaskRecommendation project={project} recommendedTasks={recommendedTasks} setRecommendedTasks={setRecommendedTasks} />
+          <div className="task-create-forms">
+            {recommendedTasks.length > 0 && recommendedTasks.map((taskTitle) => (
+              <TaskCreateForm key={taskTitle} taskTitle={taskTitle} />
+            ))}
+          </div>
         </div>
       </div>
-      </div>
-      </>
-    );
+    </>
+  );
 }
