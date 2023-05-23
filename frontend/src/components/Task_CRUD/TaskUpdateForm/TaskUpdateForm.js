c97485c904f2941ff6ed6bd11b6eef9e943a34c9
bird <mbird3@gmail.com>
2023-05-22 19:09:29 -0700

    add progress and assignee modification to task updates

--- frontend/src/components/Task_CRUD/TaskUpdateForm/TaskUpdateForm.js
+++ frontend/src/components/Task_CRUD/TaskUpdateForm/TaskUpdateForm.js
@@ -13,29 +13,49 @@ export default function TaskUpdateForm({ taskId, projectId, closeModal }) {
     const [dueDate, setDueDate] = useState('');
     const [blockingTasks, setBlockingTasks] = useState([]);
     const [errors, setErrors] = useState([]);
-    const [assignee, setAssignee] = useState('');
+    // const [assignee, setAssignee] = useState('');
+    const [assigneeId, setAssigneeId] = useState('');
     const [status, setStatus] = useState('in progress');
     const currentTask = useSelector(getTask(taskId));
     const [progress, setProgress] = useState(0);
     const project = useSelector(getProject(projectId));
     const currentUser = useSelector(state => state.session.user);
-    const statusOptions = ['not started','in progress', 'complete'];
+    const statusOptions = ['not started', 'in progress', 'complete'];
+    const [loadedTask, setLoadedTask] = useState(false);
 
     const collaborators = useSelector(state => {
         const currentProject = state.projects[projectId] || {};
         const collaboratorIds = currentProject.collaborators || [];
         return collaboratorIds.map(id => state.users[id]);
-      });
+    });
+
+    const allUsers = useSelector((state) => {
+        if (state.users) {
+            return state.users
+        }
+        else {
+            return [];
+        }
+    })
+
+    // console.log(allUsers["646c13bb28bef1e6639eafa3"], "ALL USERS");
 
     useEffect(() => {
+        // debugger;
         if (currentTask) {
+            // debugger;
+            console.log("I am here");
             setTitle(currentTask.title);
             setDescription(currentTask.description);
             setStartDate(formatDate(currentTask.startDate));
             setDueDate(formatDate(currentTask.endDate));
             setBlockingTasks(currentTask.blockingTasks || []);
+            setAssigneeId(currentTask.assignee);
+            setProgress(currentTask.progress);
+            
+            setLoadedTask(true);
         }
-    }, [currentTask, setTitle, setDescription, setDueDate])
+    }, [currentTask, setTitle, setDescription, setDueDate, setAssigneeId])
 
     useEffect(() => {
         dispatch(fetchProject(projectId))
@@ -43,7 +63,7 @@ export default function TaskUpdateForm({ taskId, projectId, closeModal }) {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const updatedTask = { ...currentTask, title, description, startDate, endDate: dueDate, blockingTasks };
+        const updatedTask = { ...currentTask, title, description, startDate, endDate: dueDate, blockingTasks, assignee: assigneeId, progress };
         // console.log(currentTask, "current task unspread");
         // console.log(...currentTask, "current task spread");
         console.log(updatedTask, "updateTask!!!!!");
@@ -55,6 +75,19 @@ export default function TaskUpdateForm({ taskId, projectId, closeModal }) {
             setErrors(err.errors);
         }
     };
+
+    function checker() {
+        debugger;
+    }
+
+    function handleAssigneeChange(e) {
+        setAssigneeId(e.target.value);
+    }
+
+    // there is no point in rendering this form unless there is a pre-existing task, therefore returning early until there is a form in which case it will continue to render the form to perform an update
+    if (!loadedTask) {
+        return null;
+    }
 
     return (
         <form onSubmit={handleSubmit} className="task-update-form">
@@ -69,7 +102,7 @@ export default function TaskUpdateForm({ taskId, projectId, closeModal }) {
             <label htmlFor="status">Status:</label>
             <select id='status' value={status} onChange={(e) => setStatus(e.target.value)}>
                 {statusOptions.map((o, ix) => {
-                return (<option value={o} key={ix}>{o}</option>)
+                    return (<option value={o} key={ix}>{o}</option>)
                 })}
             </select>
             <div>
@@ -90,16 +123,17 @@ export default function TaskUpdateForm({ taskId, projectId, closeModal }) {
                     value={dueDate}
                     onChange={(event) => setDueDate(event.target.value)} />
             </div>
-            <label htmlFor="assignee">Assignee:</label>
-            <select id="assignee" value={assignee} onChange={(e) => setAssignee(e.target.value)}>
-                <option value="">Select an assignee</option>
-                {Object.values(collaborators).map(collaborator => (
-                <option key={collaborator._id} value={collaborator._id}>{collaborator.username}</option>
-                ))}
+            <label htmlFor="assignee">Assignee</label>
+            <select id="assignee" value={assigneeId} onChange={(e) => handleAssigneeChange(e)}>
+                {Object.values(collaborators).map(collaborator => {
+                    // logIt(collaborator._id, assigneeId);
+                    return (<option key={collaborator._id} value={collaborator._id}
+                    >{collaborator.username}</option>)
+                })}
             </select>
-      <label htmlFor="progress">Progress:</label>
-      <input type="integer" id="progress" min={0} max={100} value={progress} onChange={(e) => setProgress(e.target.value)}/>
-            <label htmlFor="blockingTasks">Blocking Tasks (ctrl + click to select/deselect multiple tasks)</label> <br/>
+            <label htmlFor="progress">Progress: {progress}</label>
+                <input id="progress" type="range" min="0" max="100" step={10} value={progress} onChange={(e) => setProgress(e.target.value)} />
+            <label htmlFor="blockingTasks">Blocking Tasks (ctrl + click to select/deselect multiple tasks)</label> <br />
             <select id="blockingTasks" value={blockingTasks} onChange={(event) =>
                 setBlockingTasks(Array.from(event.target.selectedOptions, (option) => option.value))} multiple>
                 {project && project.tasks
@@ -108,7 +142,7 @@ export default function TaskUpdateForm({ taskId, projectId, closeModal }) {
                         <option key={task._id} value={task._id}>
                             {task.title}
                         </option>
-                ))}
+                    ))}
             </select>
             <button type="submit">Update Task</button>
             {errors && errors.map((error) => <div key={error}>{error}</div>)}
