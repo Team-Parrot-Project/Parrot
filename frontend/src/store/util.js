a9ffd76e68ff46768581096eee5f0f728af870e3
bird <mbird3@gmail.com>
2023-05-22 20:01:29 -0700

    update format date to handle task create

--- frontend/src/store/util.js
+++ frontend/src/store/util.js
@@ -1,9 +1,20 @@
 export function formatDate(dateString) {
-    if(dateString) {
-      const date = dateString.split("T");
-      return date[0];
+  if (dateString) {
+    let date;
+    try {
+      date = dateString.split("T");
+    } catch (error) {
+      // debugger;
+      date = new Date(dateString);
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+      const formattedDate = `${year}-${month}-${day}`;
+      return formattedDate;
     }
-    else {
-      return null;
-    }
+    return date[0];
   }
+  else {
+    return null;
+  }
+}
