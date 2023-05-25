3e69b14b9fbb9f2e3d3fec0e5fc1a831039aaa58
chrisbanas <bananas595@gmail.com>
2023-05-25 09:49:11 -0700

    fixed cut off sytle and bug so # of collabs is unique

--- frontend/src/components/UserHome/Dashboard/Dashboard.js
+++ frontend/src/components/UserHome/Dashboard/Dashboard.js
@@ -19,10 +19,12 @@ export default function Dashboard() {
   // grabs all the project collaborators from state
   const projects = useSelector(state => state.projects);
 
-  const totalCollaborators = Object.keys(projects).reduce((count, projectId) => {
-    const project = projects[projectId];
-    return count + project.collaborators.length;
-  }, 0);
+  const collaboratorsSet = Object.values(projects).reduce((collabSet, project) => {
+    project.collaborators.forEach(collab => collabSet.add(collab));
+    return collabSet;
+  }, new Set());
+
+  const totalUniqueCollaborators = collaboratorsSet.size;
 
   // grabs all completed tasks from state
   const tasks = useSelector(state => state.tasks);
@@ -91,7 +93,7 @@ export default function Dashboard() {
                     <div className="child-user-dashboard-project-task-number-ticker-container">
                       <h4
                         className="user-dashboard-project-task-number-ticker">
-                        {totalCollaborators}</h4>
+                        {totalUniqueCollaborators}</h4>
                     </div>
                   </div>
                 </div><span className="user-dashboard-project-quick-stats-title">collaborators</span>
